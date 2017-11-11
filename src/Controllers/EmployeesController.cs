deaff4dfe3a368c5c74834f1809c3af80200b766
Reginaldo Alves de Barros <R21Regis@hotmail.com>
2017-11-11 19:45:00 -0200

    add project files

---
+++ src/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using DataTablesSample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataTablesSample.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public EmployeesController()
+        {
+            EmployeeRepository.Load();
+        }
+
+        [HttpGet]
+        public IActionResult Index(DataTableModel model)
+        {
+            var result = EmployeeRepository.GetAll(model.length, model.start);
+
+            return Json(new
+            {
+                draw = model.Draw,
+                recordsTotal = result.Total,
+                recordsFiltered = result.Total,
+                data = result.Items
+            });
+        }
+    }
+}
---
+++ src/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    $('#myTable').DataTable({
+        "processing": true,
+        "serverSide": true,
+        // "ajax": "employees",
+        ajax: function (data, callback, settings) {
+            console.log(data);
+            console.log(settings);
+
+
+            var filter = {
+                draw: data.draw,
+                columns: data.columns
+            }
+
+            $.ajax({
+                url: 'employees',
+                method: 'GET',
+                data: filter
+            }).done(callback);
+        },
+        "columns": [
+            { "data": "name" },
+            { "data": "position" },
+            { "data": "office" },
+            { "data": "age" },
+            { "data": "startDate" },
+            { "data": "salary" },
+        ]
+    });
+});
